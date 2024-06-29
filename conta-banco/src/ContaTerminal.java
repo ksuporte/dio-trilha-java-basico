295ba2f80b11cc026b43df1ea784ae288d1ff0d8
Rogerio <ksuporte@gmail.com>
2024-06-29 17:17:40 -0300

    Implementacao input conta terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // Criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Agência !");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite a  Conta Bancaria !");
+        String agencia = scanner.next();
+
+        scanner.nextLine();
+        
+        System.out.println("Por favor, digite o nome do cliente !");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o valor do deposito inicial !");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
